334159f585743477cfd6a999982c7b9c981f75f9
Brandon <brandonbgrayson@gmail.com>
2022-12-18 12:51:05 -0500

    cors middleware users post route stub

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost"
+]
 
-@app.get("/login")
-async def root():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class User(BaseModel):
+    email: str
+    password: str
+
+
+@app.post("/users")
+async def root(user: User):
+    return {"user": user}
