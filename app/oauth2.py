d439693cf668a3ad14d411287b68cc927db7ed14
Brandon <brandonbgrayson@gmail.com>
2023-01-16 07:10:02 -0500

    get_current_user function added

--- app/oauth2.py
+++ app/oauth2.py
@@ -29,8 +29,8 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
 
 def verify_access_token(token: str, credentials_exception):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
-        id: str = payload.get("users_id")
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
 
         if id is None:
             raise credentials_exception
@@ -39,6 +39,8 @@ def verify_access_token(token: str, credentials_exception):
     except JWTError:
         raise credentials_exception
 
+    return token_data
+
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -46,16 +48,4 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         detail="Could Not validate credentials",
         headers={"WWW-Authenticate": "Bearer"}
     )
-
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = schemas.TokenData(id=id)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(username="")
-    if user is None:
-        raise credentials_exception
-    return user
+    return verify_access_token(token, credentials_exception)
